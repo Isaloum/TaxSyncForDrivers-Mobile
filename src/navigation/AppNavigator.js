efae0422983544be210fafe7c9873424b549aab9
Isaloum <155708736+Isaloum@users.noreply.github.com>
2026-02-02 22:48:24 -0500

    Add ReceiptsList and ReceiptAdd screens to navigator

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import ReceiptsListScreen from '../screens/ReceiptsListScreen';
+import ReceiptAddScreen from '../screens/ReceiptAddScreen';
 
 const Stack = createStackNavigator();
 
@@ -8,7 +10,8 @@ export default function AppNavigator() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        {/* Screens will be added here as we build features */}
+        <Stack.Screen name="Receipts" component={ReceiptsListScreen} />
+        <Stack.Screen name="ReceiptAdd" component={ReceiptAddScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
